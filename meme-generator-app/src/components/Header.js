9e67a7313d883802c9ad64a994df639b06bdf1ed
M. Mobasher Reza <30533488+nishatreza@users.noreply.github.com>
2022-01-08 14:17:26 +0600

    2nd commit
    2nd commit

---
+++ meme-generator-app/src/components/Header.js
@@ -0,0 +1,22 @@
+export default function Header() {
+
+    return (
+
+        <div>
+
+            <header className="header">
+
+                <img
+                    src="images/troll-face.png"
+                    className="header--image"
+                    alt=""
+                />
+                <h2 className="header--title">Meme Generator</h2>
+                <h4 className="header--project">React Course - Project 3</h4>
+            </header>
+        </div>
+
+    )
+
+
+}
