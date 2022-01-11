c1d452d9dc776f2affc30a15066bf7b810603107
M. Mobasher Reza <30533488+nishatreza@users.noreply.github.com>
2022-01-11 18:45:02 +0600

    5th
    5th

---
+++ meme-generator-app/src/prev codes/codeOne.js
@@ -0,0 +1,43 @@
+
+const [memeImage, setMemeImage] = React.useState("http://i.imgflip.com/1bij.jpg")
+
+
+
+function getMemeImage() {
+    const memesArray = memesData.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    const url = memesArray[randomNumber].url
+    setMemeImage(url)
+
+}
+
+
+
+return (
+
+    <main>
+
+        <div className="form">
+
+
+            <input
+                type="text"
+                placeholder="Top text"
+                className="form--input"
+            />
+            <input
+                type="text"
+                placeholder="Bottom text"
+                className="form--input"
+            />
+            <button
+                className="form--button"
+                onClick={getMemeImage}
+            >
+                Get a new meme image 🖼
+            </button>
+        </div>
+
+        <img src={memeImage} alt="" className="meme--image" />
+    </main>
+)
